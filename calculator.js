46b20348be2f4815ed036af8d1ff4770320251b8
Natelle Ziegler <n.ziegler@ymail.com>
2018-01-09 00:15:32 -0500

    Began creating calculator and functionality

---
+++ calculator.js
@@ -0,0 +1,112 @@
+var x = "0";
+var y = "0";
+var op = "";
+
+/**
+	Code for updating and displaying the numerical display
+**/
+var display = document.getElementById("display");
+display.innerHTML = "0";
+document.getElementById("9").addEventListener("click", numberClick);
+document.getElementById("8").addEventListener("click", numberClick);
+document.getElementById("7").addEventListener("click", numberClick);
+document.getElementById("6").addEventListener("click", numberClick);
+document.getElementById("5").addEventListener("click", numberClick);
+document.getElementById("4").addEventListener("click", numberClick);
+document.getElementById("3").addEventListener("click", numberClick);
+document.getElementById("2").addEventListener("click", numberClick);
+document.getElementById("1").addEventListener("click", numberClick);
+document.getElementById("0").addEventListener("click", numberClick);
+
+function numberClick() {
+	//If this is the first number being pressed, reset display
+	if(display.innerHTML === "0") {
+		display.innerHTML = this.innerHTML;
+	}
+	//If this is the second operand, store appropriately in y variable
+	else if(isNaN(parseInt(display.innerHTML.charAt(display.innerHTML.length - 1), 10))) {
+		y += this.innerHTML;
+		display.innerHTML += this.innerHTML;
+	}
+	else {
+		display.innerHTML += this.innerHTML;
+	}
+}
+
+/**
+	Code for manipulating the operation buttons
+	Clicking on an operation button should:
+	A) Display the symbol for that operation
+	B) Store the first operand
+	C) Store the operation being performed and wait for the second operand
+	
+**/
+document.getElementById("add").addEventListener("click", operatorClick);
+document.getElementById("subtract").addEventListener("click", operatorClick);
+document.getElementById("multiply").addEventListener("click", operatorClick);
+document.getElementById("divide").addEventListener("click", operatorClick);
+//Solving an operation
+document.getElementById("equals").addEventListener("click", function(){
+    x = parseInt(x);
+	y = parseInt(y);
+	display.innerHTML = operate(x, y, op);
+	x = display.innerHTML;
+	y = "0";
+});
+
+function operatorClick() {
+	x = display.innerHTML;
+	display.innerHTML += this.innerHTML;
+	op = this.innerHTML;
+}
+
+/**
+	Give user the option to clear the display
+**/
+document.getElementById("clear").addEventListener("click", function(){
+	display.innerHTML = 0;
+	x = "0";
+	y = "0";
+	op = "";
+});
+
+
+/**
+	Code for perfomring the operations
+**/
+
+function add (x, y) {
+	return x + y;
+}
+
+function subtract (x, y) {
+	return x - y;
+}
+
+function mutliply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	if(y === 0) {
+		return "ERROR";
+	}
+	return x / y;
+}
+
+function operate(x, y, operator) {
+	switch(operator) {
+		case "+":
+			return add(x, y);
+		break;
+		case "-":
+			return subtract(x, y);
+		break;
+		case "*":
+			return mutliply(x, y);
+		break;
+		case "/":
+			return divide(x, y);
+		break;
+	}
+}
